2b7fbe1adee285a8bd0d204cc3d47981e14745bd
nirmalks <kagosekar@gmail.com>
2024-08-14 16:41:28 +0530

    Add page not found, products module and ng bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
+
 import { Component } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [NgbCarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
+  constructor() {
+    console.log('insdie home compe')
+  }
 }
