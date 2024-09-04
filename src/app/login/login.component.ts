ef044cd835ff15e932ee8bda93f4fc142d3b0ace
nirmalks <kagosekar@gmail.com>
2024-09-04 07:35:02 +0530

    Add nullable login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup , Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatCardModule, JsonPipe],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  errorMessage = signal('');
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required]),
+  });
 
+  updateErrorMessage() {
+    const emailControl = this.loginForm.get('email')!;
+    if (emailControl.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (emailControl.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  loginSubmit() {
+    console.log(this.loginForm.value)
+    localStorage.setItem('user', JSON.stringify(this.loginForm.value));
+  }
+
+  reset() {
+    this.loginForm.reset();
+    console.log(this.loginForm.value)
+  }
 }
