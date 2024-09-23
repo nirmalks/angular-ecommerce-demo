b2e18b1eb46206137c2e3e6b09687ca0e09b7893
nirmalks <kagosekar@gmail.com>
2024-09-23 13:44:37 +0530

    Add material table with sorting, filtering and pagination

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,30 +1,83 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { map, Observable, of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import {MatTableModule} from '@angular/material/table';
+import {MatTableModule, MatTableDataSource} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.scss'
 })
 export class InventoryComponent implements OnInit {
   products: Observable<any> = of(null);
-  dataSource: any[] = [];
+  dataSource = new MatTableDataSource<any>();
   private readonly route: ActivatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  constructor() {
 
+  }
+  private _liveAnnouncer = inject(LiveAnnouncer);
+  @ViewChild(MatSort) sort: MatSort;
   ngOnInit() {
     this.products = this.route.data.pipe(map(data => {
       return data["mobiles"];
     }));
-
     this.products.subscribe((mobiles) => {
-      this.dataSource = mobiles;
+      this.dataSource.data = mobiles;
     });
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   displayedColumns: string[] = ['position', 'name', 'brand', 'price'];
 
+  length = 50;
+  pageSize = 10;
+  pageIndex = 1;
+  pageSizeOptions = [5, 10, 25];
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent: PageEvent;
+
+  handlePageEvent(e: PageEvent) {
+    //TODO handle BE req here
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
